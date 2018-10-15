d9f1817e10d87ecda6b94c2b7a57e758a16c821f
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-15 23:20:37 +0200

    Bump version to 0.2.8

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_requires = [
 
 setup(
     name='polyaxon-gpustat',
-    version='0.2.7',
+    version='0.2.8',
     license='MIT',
     description='An utility to monitor NVIDIA GPU status and usage',
     url='https://github.com/polyaxon/polyaxon-gpustat',
